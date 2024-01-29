757950db42c172298e259c5c884c20034da51bdd
kunalshukla <kun.shukla@gmail.com>
2024-01-29 13:35:07 +0000

    filter method tut in progress

--- filter.js
+++ filter.js
@@ -1,57 +1,65 @@
-  
 /**
- * To run this file in Gitpod, use the 
+ * To run this file in Gitpod, use the
  * command node filter.js in the terminal
  */
-
 
 // Simple Filtering
 const people = [
   {
-    name: 'Michael',
+    name: "Michael",
     age: 23,
   },
   {
-    name: 'Lianna',
+    name: "Lianna",
     age: 16,
   },
   {
-    name: 'Paul',
+    name: "Paul",
     age: 18,
   },
 ];
 
+// const returnPaul = people.filter((person) => person.age === 18);
+// console.log(returnPaul);
 
 // Complex Filtering
 const students = [
   {
     id: 1,
-    name: 'Mark',
-    profession: 'Developer',
+    name: "Mark",
+    profession: "Developer",
     skills: [
-      { name: 'javascript', yrsExperience: 1 },
-      { name: 'html', yrsExperience: 5 },
-      { name: 'css', yrsExperience: 3 },
-    ]
+      { name: "javascript", yrsExperience: 1 },
+      { name: "html", yrsExperience: 5 },
+      { name: "css", yrsExperience: 3 },
+    ],
   },
   {
     id: 2,
-    name: 'Ariel',
-    profession: 'Developer',
+    name: "Ariel",
+    profession: "Developer",
     skills: [
-      { name: 'javascript', yrsExperience: 0 },
-      { name: 'html', yrsExperience: 4 },
-      { name: 'css', yrsExperience: 2 },
-    ]
+      { name: "javascript", yrsExperience: 0 },
+      { name: "html", yrsExperience: 4 },
+      { name: "css", yrsExperience: 2 },
+    ],
   },
   {
     id: 3,
-    name: 'Jason',
-    profession: 'Designer',
+    name: "Jason",
+    profession: "Designer",
     skills: [
-      { name: 'javascript', yrsExperience: 1 },
-      { name: 'html', yrsExperience: 1 },
-      { name: 'css', yrsExperience: 5 },
-    ]
+      { name: "javascript", yrsExperience: 1 },
+      { name: "html", yrsExperience: 1 },
+      { name: "css", yrsExperience: 5 },
+    ],
   },
 ];
+
+const filterByExp = students.filter((student) => {
+  const strongSkills = student["skills"].filter(
+    (item) => item.yrsExperience >= 5
+  );
+  return strongSkills.length > 0;
+});
+console.log(filterByExp);
