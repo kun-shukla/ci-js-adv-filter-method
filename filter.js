bf2b411372b6443359a3d87dc7029d865759ce9d
Kunal <kun.shukla@gmail.com>
2024-01-29 15:39:10 +0000

    filter challenge complete

--- filter.js
+++ filter.js
@@ -79,6 +79,38 @@ console.log(candNames)
 //   })
   
 //   console.log(strongExp)
-  
+
+// filter challenge
+
+let pupils = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 80, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 84, maths: 97, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 67, english: 87, art: 95},
+  }
+];
+
+const hasStrongMathsSkills = pupils.filter(pupil => pupil.results.maths >= 90)
+
+console.log(hasStrongMathsSkills)
 
 
